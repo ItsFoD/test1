1c951a597bf72ddb9c7fcc46dcbbad4662865558
fod <sonaahmed01@gmail.com>
2022-11-12 18:40:12 +0200

    commit for tag?

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 print("ayo")
 print("changed changed")
+<<<<<<< Updated upstream
 print ("test test stash stash")
 print("commit pls")
+=======
+print ("test test stash stash")
+>>>>>>> Stashed changes
+print ("idk?")
